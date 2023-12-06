49919e41f00e78d0975ebfad1b73eab0a2de12b3
Weriks Santos <werikscs@gmail.com>
2023-12-06 23:08:32 +0000

    feat: implement ProductListView filter

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
-  const productList = [
-    {id: 1, name: 'Product 1', price: 100},
-    {id: 2, name: 'Product 2', price: 200},
-    {id: 3, name: 'Product 3', price: 300},
-    {id: 4, name: 'Product 4', price: 400},
+  import { computed, ref } from 'vue';
+  import ProductList from '../entities/ProductList';
+
+  const productList = new ProductList()
+  productList.products = [
+    {name: 'Product1'},
+    {name: 'Product2'},
+    {name: 'Product3'},
+    {name: 'Product4'},
   ]
+  const productNameToSearch = ref('')
+  const filteredProducts = computed(() => {
+    if(productNameToSearch.value === '') return productList.getProducts()
+    const product = productList.getProductByName(productNameToSearch.value)
+    return product ? [product] : []
+  })
 </script>
 
 <template>
   <h1>Product List</h1>
-  <ul>
-    <li v-for="product in productList" :key="product.id">
-      {{ product.name }}
+  <input v-model="productNameToSearch" placeholder="Type a product name">
+  <p v-if="!filteredProducts.length">Product not found</p>
+  <ul v-for="product in filteredProducts" :key="product?.name">
+    <li v-if="filteredProducts.length">
+      {{ product?.name }}
     </li>
   </ul>
 </template>
