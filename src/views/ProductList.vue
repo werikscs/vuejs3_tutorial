d2be388f7189cab3af44e4ea93bc9e1b30d8c55e
Weriks Santos <werikscs@gmail.com>
2023-12-06 20:06:24 +0000

    feat: add basic product list

---
+++ src/views/ProductList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  const productList = [
+    {id: 1, name: 'Product 1', price: 100},
+    {id: 2, name: 'Product 2', price: 200},
+    {id: 3, name: 'Product 3', price: 300},
+    {id: 4, name: 'Product 4', price: 400},
+  ]
+</script>
+
+<template>
+  <h1>Product List</h1>
+  <ul>
+    <li v-for="product in productList" :key="product.id">
+      {{ product.name }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
