5c866d450e3c041a657525965b07ff44508ca2d3
Weriks Santos <werikscs@gmail.com>
2023-12-07 23:39:41 +0000

    chore: add happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-		globals: true
-	},
   plugins: [vue()],
+  test: {
+		globals: true,
+    environment: "happy-dom"
+	},
 })
